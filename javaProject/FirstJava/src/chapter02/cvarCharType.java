c01d831adf55cb83e35b5f14631e6263ce5a2e66
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-03-21 18:22:04 +0900

    기본형타입 변수

---
+++ javaProject/FirstJava/src/chapter02/cvarCharType.java
@@ -0,0 +1,21 @@
+package chapter02;
+
+public class cvarCharType {
+
+	public static void main(String[] args) {
+		
+		char ch1 = 'A';
+		
+		System.out.println(ch1);
+		
+		int number = ch1 + 0;
+		
+		System.out.println(number);
+		
+		char ch2 = 65;
+		
+		System.out.println(ch2);
+		
+	}
+
+}
