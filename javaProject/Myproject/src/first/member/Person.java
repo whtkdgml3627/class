c7441bc3a7b49206b4cc41a7506aba22c788ad11
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-03-24 16:34:05 +0900

    자바 예제 문제 풀이

---
+++ javaProject/Myproject/src/first/member/Person.java
@@ -0,0 +1,17 @@
+package first.member;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		String memberName = "이강인";
+		
+//		String memberPhoneNumber = "010-1111-7777";
+		String memPhoneNm = "010-1111-7777";
+		
+		String personNumber = "991214-1111111";
+		long pNum = 9912141111111L;
+		
+	}
+
+}
