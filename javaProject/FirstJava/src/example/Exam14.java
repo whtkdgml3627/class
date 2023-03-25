00a86029cc3d243877ebeb37c9a9d5b387f4c2d5
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-03-25 19:24:08 +0900

    Update Exam14.java
    자바 예제문제 복습

--- javaProject/FirstJava/src/example/Exam14.java
+++ javaProject/FirstJava/src/example/Exam14.java
@@ -12,6 +12,16 @@ public class Exam14 {
 			
 			System.out.println("A: "+ a +", B: "+ b);
 		}
+		
+		for(int i = 0; i < 10; i++) {
+//			a가 i인 이유 : 0 부터 9까지 증가하기 때문
+			int a = i;
+//			b는 a에서 9를 뺀 값을 구하면 되기 때문
+//			ex) a가 1일때 9-1=8 이면 b가 8이 되면 a+b=9이기 때문
+			int b = 9 - a;
+			
+			System.out.println(a + " " + b + " = " + (a + b) + "" + (b + a));
+		}
 	}
 
 }
