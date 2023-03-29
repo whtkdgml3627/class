605a2b8c3ec35098d091f06841ceda07cd7a94bd
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-03-29 17:09:18 +0900

    자바 문제풀이 소스 수정

--- javaProject/Myproject/src/chapter06/StudentMain.java
+++ javaProject/Myproject/src/chapter06/StudentMain.java
@@ -13,16 +13,16 @@ public class StudentMain {
 		
 		Student[] stArr = new Student[10];
 		
-		stArr[0] = new Student("조상희0", 100, 99, 80);
-		stArr[1] = new Student("조상희1", 90, 98, 81);
-		stArr[2] = new Student("조상희2", 80, 97, 82);
-		stArr[3] = new Student("조상희3", 70, 96, 83);
-		stArr[4] = new Student("조상희4", 60, 95, 84);
-		stArr[5] = new Student("조상희5", 50, 94, 85);
-		stArr[6] = new Student("조상희6", 60, 93, 86);
-		stArr[7] = new Student("조상희7", 70, 92, 87);
-		stArr[8] = new Student("조상희8", 80, 91, 88);
-		stArr[9] = new Student("조상희9", 90, 90, 89);
+		stArr[0] = new Student("조상희0", 90, 99, 80);
+		stArr[1] = new Student("조상희1", 80, 98, 81);
+		stArr[2] = new Student("조상희2", 70, 97, 82);
+		stArr[3] = new Student("조상희3", 60, 96, 83);
+		stArr[4] = new Student("조상희4", 50, 95, 84);
+		stArr[5] = new Student("조상희5", 40, 94, 85);
+		stArr[6] = new Student("조상희6", 50, 93, 86);
+		stArr[7] = new Student("조상희7", 60, 92, 87);
+		stArr[8] = new Student("조상희8", 70, 91, 88);
+		stArr[9] = new Student("조상희9", 80, 90, 89);
 		
 		for(Student student : stArr) {
 			System.out.println(student);
