f22f7e8630eb39e10cbb6c5451d4ded295af34a4
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-04-04 17:32:04 +0900

    자바 예외처리 예제문제 풀이 수정

--- javaProject/Myproject/src/chapter10/Person.java
+++ javaProject/Myproject/src/chapter10/Person.java
@@ -7,6 +7,7 @@ public class Person {
 
 	public static void main(String[] args) {
 		
+		// 1. 콘솔에서 사용자 아이디를 입력 받아 정상적인 영문자와 숫자로만 이루어진 값을 입력했는지 확인하는 프로그램을 만들어봅시다. 
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("사용자 아이디를 입력해주세요.");
@@ -23,7 +24,9 @@ public class Person {
 		}catch(BadIdInputException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
+		// 2. Scanner 클래스로 태어난 년도를 입력 받을 때 int 타입으로 받도록 합시다.
+		// 이때 nextInt() 메소드를 사용하게 되는데 이때 발생하는 예외처리를 하도록 합시다. 
 		System.out.println("태어난 년도를 입력해주세요.");
 		int year = 0;
 		
@@ -31,6 +34,10 @@ public class Person {
 			year = sc.nextInt();
 		}catch(InputMismatchException e) {
 			System.out.println("숫자가 아닌 값을 입력했습니다.");
+		}catch(Exception e){
+			System.out.println("예기치 못한 오류 발생!");
+		} finally {
+			sc.close();
 		}
 		
 	}
