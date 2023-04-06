13498ed429be04f161ffce7b3d8775990b7dca7d
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-04-06 12:40:44 +0900

    자바 Set<E> 예제 풀이

---
+++ javaProject/FirstJava/src/chapter11/HashSetTest.java
@@ -0,0 +1,39 @@
+package chapter11;
+
+import java.util.HashSet;
+
+public class HashSetTest {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> set = new HashSet<>();
+		
+		Object o;
+		
+		Integer i;
+		
+		set.add("손흥민");
+		set.add("박지성");
+		set.add("이강인");
+		set.add("손흥민");
+		set.add("이강인");
+		
+		System.out.println("요소의 개수 : " + set.size());
+		for(String name : set) {
+			System.out.println(name);
+		}
+		
+		HashSet<Integer> numbers = new HashSet<>();
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(10);
+		numbers.add(30);
+		
+		for(Integer num : numbers) {
+			System.out.println(num);
+		}
+		
+	}
+
+}
