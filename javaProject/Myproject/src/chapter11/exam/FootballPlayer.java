bdeaf47b59b9275c59e1d4ac43db12b6ba41a513
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-04-06 17:08:30 +0900

    자바 컬렉션 Set (hashSet/TreeSet), Map(hashMap) 문제 풀이

--- javaProject/Myproject/src/chapter11/exam/FootballPlayer.java
+++ javaProject/Myproject/src/chapter11/exam/FootballPlayer.java
@@ -2,27 +2,104 @@ package chapter11.exam;
 
 // 1. 축구선수 인스턴스를 저장할 수 있는 List<E> 컬렉션 인스턴스를 생성해서 인스턴스를 저장하고 출력하는 프로그램을 만들어 봅시다.
 
-public class FootballPlayer {
+public class FootballPlayer implements Comparable<FootballPlayer> {
 	
 	// 값을 받을 인스턴스 변수 선언
-	String name;
-	String number;
-	String team;
-	String age;
-	
+	private String name;
+	private int number;
+	private String team;
+	private int age;
+
 	// 생성자 생성해서 초기화
-	public FootballPlayer(String name, String number, String team, String age) {
-		super();
+	public FootballPlayer(String name, int number, String team, int age) {
 		this.name = name;
 		this.number = number;
 		this.team = team;
 		this.age = age;
 	}
 	
+	// 생성자 생성해서 초기화
+	public FootballPlayer() {
+		// 기본 생성자
+	}
+	
+	// get/set
+	// 이름
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	// 번호
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	// 팀
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	// 나이
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	// Set<E>를 사용하면 hashCode를 찾는다?
+	// hashCode를 사용해서 중복되는 값 찾기
+	// 나이가 같은값을 찾아서 equals로 보내기
+	@Override
+	public int hashCode() {
+		return this.age;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// 초기값 false 지정
+		boolean result = false;
+		
+		// obj가 널이 아니면서 형변환이 가능할 때
+		if(obj != null && obj instanceof FootballPlayer) {
+			FootballPlayer fp = (FootballPlayer)obj;
+			
+			// 나이가 같으면서 이름과 팀이 같으면 true값을 넘겨줘서 중복제거 처리
+			if(name.equals(fp.getName()) && team.equals(fp.getTeam())) {
+				result = true;
+			}
+		}
+		return result;
+	}
+	
+	// 정렬을 해주기 위해서는 Comparable인터페이스를 implements 구현 시켜줘야 함
+	// 구현 시켜주고 나면 compareTo 메소드를 오버라이딩 시켜줘야 오류가 안남
+	// 값이 크면 양수, 작으면 음수, 같으면 0
+	@Override
+	public int compareTo(FootballPlayer o) {
+		int result = 0;
+		if(team.compareTo(o.getTeam()) != 0) {
+			result = team.compareTo(o.getTeam());
+		}else if(team.compareTo(o.getTeam()) == 0 && name.compareTo(o.getName()) != 0) {
+			result = name.compareTo(o.getName());
+		}else {
+			result = Integer.compare(number, o.getNumber());
+		}
+		return result;
+	}
+	
 	// 출력을 하려는 메소드 생성
 	// 메소드를 생성하였으니 main에서 호출해서 출력한다.
 	public void playerShow() {
-		System.out.print("이름 : " + this.name + "\t번호 : " + this.number + "\t팀 : " + this.team + "\t나이 : " + this.age + "\n");
+		System.out.print("이름 : " + this.name + "\t번호 : " + this.number + "\t\t팀 : " + this.team + "\t나이 : " + this.age + "\n");
 	}
+
+
 	
 }
--- javaProject/Myproject/src/chapter11/exam/FootballPlayerMain.java
+++ javaProject/Myproject/src/chapter11/exam/FootballPlayerMain.java
@@ -1,7 +1,12 @@
 package chapter11.exam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 // 1. 축구선수 인스턴스를 저장할 수 있는 List<E> 컬렉션 인스턴스를 생성해서 인스턴스를 저장하고 출력하는 프로그램을 만들어 봅시다.
 
@@ -20,10 +25,10 @@ public class FootballPlayerMain {
 		// 값을 넣을때는 add를 사용한다.
 		// add안에는 참조변수를 선언해서 넣어준다.
 		// 참조변수로 사용하고자 하는 클래스의 생성자를 불러온다.
-		list.add(new FootballPlayer("손흥민", "7번", "토트넘", "30살"));
-		list.add(new FootballPlayer("호날두", "8번", "레알마드리드", "30살"));
-		list.add(new FootballPlayer("메시", "7번", "바르셀로나", "30살"));
-		list.add(new FootballPlayer("음바페", "9번", "파리생제르망", "30살"));
+		list.add(new FootballPlayer("손흥민", 7, "토트넘", 30));
+		list.add(new FootballPlayer("호날두", 8, "레알마드리드", 30));
+		list.add(new FootballPlayer("메시", 7, "바르셀로나", 30));
+		list.add(new FootballPlayer("음바페", 9, "파리생제르망", 30));
 		
 		// ArrayList를 향상된 for문으로 출력한다.
 		// 출력할 때 FootballPlayer의 메소드를 사용할 것 이므로
@@ -32,8 +37,75 @@ public class FootballPlayerMain {
 			f.playerShow();
 		}
 		
+		System.out.println("============================Set============================");
+		// 2. 축구선수의 인스턴스가 팀과 이름 그리고 나이가 같으면 같은 선수라 판단하고
+		// 입력이 되지 않도록 Set<E> 컬렉션을 이용해서 축구선수 인스턴스를 저장하고 출력하는 프로그램을 만들어 봅시다.
 		
+		// Set<E>인 HashSet<E>를 인스턴스로 생성
+		// set 값을 null로 먼저 초기화
+		Set<FootballPlayer> set = null;
+		set = new HashSet<FootballPlayer>();
 		
+		// set에 값을 add로 넣어 준다.
+		set.add(new FootballPlayer("손흥민", 7, "토트넘", 30));
+		set.add(new FootballPlayer("손흥민", 7, "토트넘", 30));
+		set.add(new FootballPlayer("케인", 8, "토트넘", 28));
+		set.add(new FootballPlayer("손흥민", 8, "토트넘", 25));
+		set.add(new FootballPlayer("손흥민", 7, "토트넘", 28));
+		set.add(new FootballPlayer("요리스", 1, "토트넘", 30));
+		
+		for(FootballPlayer s : set) {
+			s.playerShow();
+		}
+
+		System.out.println("==========================TreeSet==========================");
+		
+		// 3. TreeSet<E>을 이용해서 팀 이름순으로 정렬하고, 같은 팀의 선수들은 이름 순으로 정렬하고,
+		// 같은 이름의 선수는 번호 순으로 저장하는 프로그램을 만들어 봅시다.
+		Set<FootballPlayer> tSet = null;
+		tSet = new TreeSet<>();
+
+		// tSet에 값을 add로 넣어준다.
+		tSet.add(new FootballPlayer("손흥민", 7, "토트넘", 30));
+		tSet.add(new FootballPlayer("호날두", 8, "레알마드리드", 30));
+		tSet.add(new FootballPlayer("메시", 7, "바르셀로나", 30));
+		tSet.add(new FootballPlayer("음바페", 9, "파리생제르망", 30));
+		tSet.add(new FootballPlayer("케인", 8, "토트넘", 28));
+		tSet.add(new FootballPlayer("손흥민", 10, "토트넘", 30));
+		tSet.add(new FootballPlayer("요리스", 1, "토트넘", 30));
+		
+		for(FootballPlayer t : tSet) {
+			t.playerShow();
+		}
+
+		System.out.println("==========================HashMap==========================");
+		
+		// 4. 축구선수의 번호를 key로 하고 축구선수 인스턴스를 저장하는 Map<K,V> 인스턴스를 이용해서 프로그램을 만들어봅시다
+		Map<Integer, FootballPlayer> map = new HashMap<>();
+		// 인스턴스 생성하여 값 저장
+		FootballPlayer fp1 = new FootballPlayer("손흥민", 7, "토트넘", 30);
+		FootballPlayer fp2 = new FootballPlayer("호날두", 8, "레알마드리드", 30);
+		FootballPlayer fp3 = new FootballPlayer("메시", 7, "바르셀로나", 30);
+		FootballPlayer fp4 = new FootballPlayer("음바페", 9, "파리생제르망", 30);
+		
+		// mpa은 put으로 값을 넣어준다
+		// key값 getNumber()를 넣어서 fp1~4 인스턴스를 map에 넣어준다
+		map.put(fp1.getNumber(), fp1);
+		map.put(fp2.getNumber(), fp2);
+		map.put(fp3.getNumber(), fp3);
+		map.put(fp4.getNumber(), fp4);
+
+		// get으로 값을 가져온다
+		// 손흥민, 메시 번호가 중복되므로 뒤에 있는 값인 메시가 노출된다.
+		map.get(fp1.getNumber()).playerShow();
+		map.get(fp2.getNumber()).playerShow();
+		map.get(fp3.getNumber()).playerShow();
+		map.get(fp4.getNumber()).playerShow();
+		
+		Set<Integer> keySet = map.keySet();
+		for(Integer m : keySet) {
+			System.out.println(m);
+		}
 	}
 
 }
