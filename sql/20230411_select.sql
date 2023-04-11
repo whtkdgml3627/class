379b784e7c5f798f5919d63fb8805ac67b9f10cd
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-04-11 13:18:44 +0900

    DB select문 조회,연산 이용하여 출력 예제

--- sql/20230411_select.sql
+++ sql/20230411_select.sql
@@ -44,5 +44,59 @@ SELECT  dname --, deptno, loc
 FROM dept
 ;
 
+-- select 구문의 프리젠테이션영역에 테이블이 가지는 컬럼뿐 아니라 연산(함수)의 결과를 새로운 컬럼으로 출력
+select sal, sal+100, sal-100, sal*10, sal/2 from emp;
+select sal+100 from emp;
+select sal-100 from emp;
+select sal*10 from emp;
+select sal/2 from emp;
+
+-- 사원테이블에서 사원의 이름과 급여, 연봉계산 결과를 출력 (연봉의 계산은 급여*12)
+select ename, sal, sal*12+500 from emp;
+
+-- 상여금 계산 > sal*12+comm
+-- null의 연산결과는 null이 나온다
+select sal, comm, sal*12+comm from emp where comm is not null;
+select sal, comm, nvl(comm,0), sal*12+nvl(comm,0) from emp;
+
+-- 컬럼의 이름에 별칭 부여
+-- 컬럼뒤에 as 키워드를 쓰고 별칭을 작성
+select sal, comm, nvl(comm,0) as comm1, sal*12+nvl(comm,0) as yearSal from emp;
+
+-- 문자열의 연산 > 문자열을 붙여준다 : 문자열 || 문자열
+select ename || ' is a ' || job as person from emp;
+
+-- select [distinct] from ...
+-- distinct의 역할 : 중복값을 제거하고 값을 하나만 출력해야할 때 사용
+select deptno from emp;
+select distinct deptno from emp;
+select job from emp;
+select distinct job from emp;
+
+-- 원하는 행 검색
+-- where 절을 사용한다.
+-- select ... from ... where 조건식;
+-- 급여가 3000 이상인 사원의 정보 리스트
+select * from emp where sal >= 3000;
+
+-- = : 컬럼의 데이터가 같은지 비교 (JAVA의 == 와 동일)
+-- 10번 부서에 소속된 사원들의 정보를 출력
+select ename, job, deptno from emp where deptno = 10;
+
+-- 다음은 이름이 FORD인 사원의 사원번호(EMPNO)과 사원이름(ENAME)과 급여(SAL)을 출력
+-- 문자열 비교 > '' 작은 따옴표 사용
+select empno, ename, sal from emp where ename = 'FORD';
+
+-- 날짜를 비교할 때에도 홑 따옴표를 사용한다.
+select * from emp where hiredate = '1981-12-03';
+-- 날짜를 크다 작다로 이후 이전을 비교해줄 수 있다.
+select * from emp where hiredate > '1981-12-03';
+
+-- 논리연산자 : AND, OR, NOT
+-- 10번부서의 매니저가 누구인지 찾아보자
+select * from emp where deptno=10 and job='MANAGER';
+select * from emp where deptno=10 or job='MANAGER';
+select * from emp where not deptno=10;
+
 
 
