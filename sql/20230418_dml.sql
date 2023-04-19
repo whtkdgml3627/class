acd7a3737600f7adcbad77719ba0d14a865eb4e0
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-04-19 11:47:08 +0900

    DB ddl,dml복습 및 sequence 작성

--- sql/20230418_dml.sql
+++ sql/20230418_dml.sql
@@ -97,3 +97,18 @@ select * from emp01;
 -- 사원 테이블에서 부서명이 SALES인 사원을 모두 삭제해봅시다.
 select deptno from dept02 where dname='SALES';
 delete from emp01 where deptno=(select deptno from dept02 where dname='SALES');
+
+-- sequence : 연속된 숫자를 생성해주는 객체
+-- create sequence 이름 [옵션]
+drop sequence seq_dept_deptno;
+create sequence seq_dept_deptno
+start with 10
+increment by 10;
+
+-- 자동 생성된 값 : 시퀀스객체이름.nextval
+
+select seq_dept_deptno.nextval from dual;
+insert into dept01 values (seq_dept_deptno.nextval, 'text이름', 'test위치');
+select * from dept01;
+select seq_dept_deptno.currval from dual;
+
