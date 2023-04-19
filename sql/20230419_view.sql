ddf549ee4371921dbe23e88e0c8460ad94a7c07c
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-04-19 12:51:15 +0900

    DB view 생성 예제

---
+++ sql/20230419_view.sql
@@ -0,0 +1,54 @@
+-- 2023.04.19
+
+-- VIEW : 가산 테이블
+-- VIEW 객체는 sql을 가진다 -> sql 실행결과를 가상의 테이블로 사용
+
+select empno, ename, deptno
+from emp
+where deptno=30;
+
+-- view 객체 생성
+-- create or replace view 뷰이름 as 서브쿼리
+create or replace view emp_view30
+as
+select empno, ename, deptno
+from emp
+where deptno=30;
+
+select * from emp_view30;
+
+-- 주문정보 : orders, book, customer
+select o.orderid, c.name, b.bookname, b.price, o.saleprice, o.orderdate, c.phone
+from orders o join book b
+on o.bookid=b.bookid
+join customer c
+on o.custid=c.custid;
+
+create or replace view order_info
+as
+select o.orderid, c.name, b.bookname, b.price, o.saleprice, o.orderdate, c.phone
+from orders o join book b
+on o.bookid=b.bookid
+join customer c
+on o.custid=c.custid;
+
+select bookname from order_info where name='박지성';
+
+select view_name, text from user_views;
+
+-- view 삭제
+drop view emp_view30;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
