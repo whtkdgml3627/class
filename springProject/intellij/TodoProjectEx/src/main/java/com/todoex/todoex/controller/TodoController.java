e80aab5432569bc3e0d981e81b4e510debe52822
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-05-10 18:18:30 +0900

    spring todo 예제 셋팅

---
+++ springProject/intellij/TodoProjectEx/src/main/java/com/todoex/todoex/controller/TodoController.java
@@ -0,0 +1,9 @@
+package com.todoex.todoex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Log4j2
+public class TodoController {
+}
