c75474b194a9dcda942593cd768197a005b7e51d
whtkdgml3627 <whtkdgml36271@gmail.com>
2023-05-15 16:07:45 +0900

    spring pathvariable, interceptor 예제

--- springProject/intellij/SpringMVC/src/main/java/com/hi/app/controller/LoginController.java
+++ springProject/intellij/SpringMVC/src/main/java/com/hi/app/controller/LoginController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping(value = "/login")
 @Log4j2
@@ -18,8 +21,14 @@ public class LoginController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String login(){
+	public String login(HttpServletRequest request){
 		log.info("post  |  /login");
+
+		// 로그인 처리
+		HttpSession session = request.getSession();
+		session.setAttribute("loginInfo", "Login");
+		log.info("로그인 처리");
+
 		// post 처리 후 redirect
 		return "redirect:/sample/sample1";
 		// http://localhost:8080/sample/sample1
---
+++ springProject/intellij/SpringMVC/src/main/webapp/WEB-INF/views/member/info.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hi
+  Date: 2023-05-15
+  Time: 오후 2:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+	<title>Member Info</title>
+</head>
+<body>
+
+<h1>idx : ${idx}</h1>
+<h1>type : ${type}</h1>
+
+</body>
+</html>
